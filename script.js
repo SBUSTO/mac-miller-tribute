32dde37f321b71295c3aa075d5f8e1ec4a3c1f8f
SBUSTO <sbj2001@outlook.com>
2024-03-23 14:29:24 -0400

    first attempt

---
+++ script.js
@@ -0,0 +1,48 @@
+$(window).scroll(function(){         
+    $('.container p').each(function(){
+        var scrollTop     = $(window).scrollTop(),
+            elementOffset = $(this).offset().top,
+            distance      = (elementOffset - scrollTop),
+            windowHeight  = $(window).height(),
+            breakPoint    = windowHeight*0.9;
+  
+        if(distance > breakPoint) {
+          $(this).addClass("more-padding"); 
+        }  if(distance < breakPoint) {
+          $(this).removeClass("more-padding");  
+        }
+    });
+  });
+  
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
+
+ 
